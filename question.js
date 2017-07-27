9e33d38b72101e7b0278a1835b173cb2595ada7e
James Houghton <jamesthoughton@gmail.com>
2017-07-27 16:57:30 -0400

    Create functionality but animations are choppy

--- question.js
+++ question.js
@@ -1,10 +1,15 @@
-function QuestionMark(abscissa, ordinate, color) {
-	this.x = abscissa || Math.random() < .5 ? Math.random() * (-100) : Math.random() * 100 + window.innerWidth;
-	this.y = ordinate || Math.random() < .5 ? Math.random() * (-100) : Math.random() * 100 + window.innerHeight;
+function QuestionMark(argument, color) {
+	this.arg = argument || Math.random() * 2 * Math.PI;
+	this.x = QuestionMark.radius * Math.cos(this.arg) + window.innerWidth/2;
+	this.y = QuestionMark.radius * Math.sin(this.arg) + window.innerHeight/2;
 	this.color = color || QuestionMark.randomColor();
 	this.el = document.createElement("span");
 	this.el.className = "question-mark";
 	this.prepareElement();
+	this.animate();
+	setTimeout(function() {
+		this.destroy();
+	}, QuestionMark.lifetime);
 };
 QuestionMark.prototype.prepareElement = function() {
 	this.el.style.left = this.x + "px";
@@ -12,12 +17,21 @@ QuestionMark.prototype.prepareElement = function() {
 	this.el.style.color = this.color;
 	QuestionMark.container.appendChild(this.el);
 };
-QuestionMark.randomColor = function(bitDepth=8) {
-	return "rgb(" + [... new Array(3)].map(function() { return Math.floor(Math.pow(2, bitDepth) * Math.random()); }).join(',') + ")";
+QuestionMark.prototype.animate = function() {
+	this.el.style.left = (window.innerWidth  - this.x) + "px";
+	this.el.style.top  = (window.innerHeight - this.y) + "px";
+}
+QuestionMark.randomColor = function(offset=50) {
+	return "rgb(" + [... new Array(3)].map(function() { return offset+Math.floor((256-offset) * Math.random()); }).join(',') + ")";
 };
 QuestionMark.container = null;
+QuestionMark.radius = null;
+QuestionMark.lifetime = 10000;
 
 window.onload = function() {
+	QuestionMark.radius = Math.sqrt(Math.pow(window.innerHeight, 2) + Math.pow(window.innerWidth, 2)) + 250;
 	QuestionMark.container = document.getElementById("question-marks");
-	var q = new QuestionMark();
+	setInterval(function() {
+		new QuestionMark();
+	}, 10);
 };
